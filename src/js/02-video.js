90cdec5d70ba60e13b0d4919f6b93915d7fe9582
lotickles <148078790+lotickles@users.noreply.github.com>
2024-04-27 19:39:16 +0800

    task2 of HW8 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,27 +4,30 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe#vimeo-player');
 const player = new Player(iframe);
 
+const TIME_KEY = 'videoplayer-current-time';
+const currentTime = localStorage.getItem(TIME_KEY) || 0;
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+    // console.log('currenttime player second', seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
 const timeFunction = data => {
-  let playerSecond = data.seconds;
-  localStorage.setItem('videoplayer-current-time', playerSecond);
-
-  player.on('timeupdate', throttle(timeFunction, 1000));
-
-  player
-    .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-    .then(function (seconds) {
-      // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-
-        default:
-          // some other error occurred
-          break;
-      }
-    });
+  const playerSecond = data.seconds;
+  // console.log('player second', playerSecond);
+  localStorage.setItem(TIME_KEY, playerSecond);
 };
 player.on('timeupdate', throttle(timeFunction, 1000));
